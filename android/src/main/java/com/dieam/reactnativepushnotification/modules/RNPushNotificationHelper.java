11cce050963d7d0052df146fd5f6b50c65afc9e4
Khoi Nguyen <akhoi90@gmail.com>
2017-10-25 14:51:47 +0700

    Missing import

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -19,6 +19,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReadableMap;
